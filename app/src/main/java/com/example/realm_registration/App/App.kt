17cefd82a25690e1c86b1b9a90b99f22216b29cd
OLIMJON <olimjonrustamov3477@gmail.com>
2021-04-15 19:32:36 +0500

    realm'dan tashqari hammasi yaxshi

--- app/src/main/java/com/example/realm_registration/App/App.kt
+++ app/src/main/java/com/example/realm_registration/App/App.kt
@@ -1,11 +1,11 @@
 package com.example.realm_registration.App
 
 import android.app.Application
-import io.realm.Realm
+//import io.realm.Realm
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        Realm.init(this)
+//        Realm.init(this)
     }
 }
--- app/src/main/java/com/example/realm_registration/RealmObjects/Contact.kt
+++ app/src/main/java/com/example/realm_registration/RealmObjects/Contact.kt
@@ -1,8 +1,8 @@
 package com.example.realm_registration.RealmObjects
-
-import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
-import io.realm.annotations.Required
+//
+//import io.realm.RealmObject
+//import io.realm.annotations.PrimaryKey
+//import io.realm.annotations.Required
 
 open class Contact(
     ism_familya: String,
@@ -10,23 +10,22 @@ open class Contact(
     davlat: String,
     manzil: String,
     parol: String,
-    image_path: String
-):RealmObject() {
-    @PrimaryKey
-    var tel_raqam:String=tel_raqam
-
-    @Required
-    var ism_familya: String = ism_familya
-
-    @Required
-    var davlat: String = davlat
-
-    @Required
-    var manzil: String = manzil
-
-    @Required
-    var parol: String = parol
-
-    @Required
-    var image_path: String = image_path
-}
+    image_path: String)
+//):RealmObject() {
+//    @PrimaryKey
+//    var tel_raqam:String=tel_raqam
+//
+//    @Required
+//    var ism_familya: String = ism_familya
+//
+//    @Required
+//    var davlat: String = davlat
+//
+//    @Required
+//    var manzil: String = manzil
+//
+//    @Required
+//    var parol: String = parol
+//
+//    @Required
+//    var image_path: String = image_path
